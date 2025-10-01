7ae8988457a76b83cd6b8a7e902b1e1bdbc2e8bd
Larisa <79214239+LarisaShatil@users.noreply.github.com>
2025-10-01 10:06:27 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer>
       <Link href="https://github.com/LarisaShatil"
-        target="_blank" fontWeight='700' color='blue.700' _hover={{ color: 'white' }} >      2023 &copy; Larisa Shatillo
+        target="_blank" fontWeight='700' color='blue.700' _hover={{ color: 'white' }} >      2025 &copy; Larisa Shatillo
       </Link>
     </footer>
   );
